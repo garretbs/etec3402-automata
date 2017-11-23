e4adb93e0f79f4fce4943464c4a9cf87a0ca99ef
garretbs <garretbs@gmail.com>
2017-11-23 15:49:47 -0500

    Simulate finite automata to determine if an input string matches C language constructs

--- assignment_5.py
+++ assignment_5.py
@@ -1,113 +1,193 @@
 #Garret Stevens
-alphabet = "abc"
 
-def a(str):
-	state = 0;
-	final = [2]
-	for c in str:
-		if c not in alphabet:
-			return False
+def a():
+	state = 0
+	final = [4, 5]
+	for c in stra:
 		if state == 0:
-			if c == 'a':
+			if c == '/':
 				state = 1
+			else:
+				return False
 		elif state == 1:
-			if c == 'a':
-				pass
-			elif c == 'b':
-				state = 2
-			elif c == 'c':
-				state = 0
-		elif state == 2:
-			if c == 'a':
-				state = 1
-			else:
-				state = 0
-	if state in final:
-		return True
-	return False
-	
-def b(str):
-	state = 0;
-	final = [3]
-	for c in str:
-		if c not in alphabet:
-			return False
-		if state < 3:
-			if c == 'a':
-				state += 1
-			else:
-				state = 0
-	if state in final:
-		return True
-	return False
-	
-def c(str):
-	state = 0;
-	final = [0, 1, 2, 3]
-	for c in str:
-		if c not in alphabet:
-			return False
-		if state == 0: #start state
-			if c == 'a':
-				state = 1
-			elif c == 'b':
-				state = 2
-			elif c == 'c':
-				state = 3
-		if state == 1: #got an a
-			if c == 'b' or c == 'c':
-				state = 4
-		if state == 2: #got a b
-			if c == 'a' or c == 'c':
+			if c == '/':
 				state = 5
-		if state == 3: #got a c
-			if c == 'a' or c == 'b':
-				state = 6
-		if state == 4: #a and then something else
-			if c == 'a':
-				state = 1
-		if state == 5: #b and then something else
-			if c == 'b':
+			elif c == '*':
 				state = 2
-		if state == 6: #c and then something else
-			if c == 'c':
+		elif state == 2:
+			if c == '*':
 				state = 3
+		elif state == 3:
+			if c == '/':
+				state = 4
+			elif c == '*':
+				pass
+			else:
+				state = 2
+		elif state == 4:
+			return False
 	if state in final:
 		return True
 	return False
-	
-def d(str):
-	state = 0;
-	final = [3]
-	for c in str:
-		if c not in alphabet:
-			return False
-		if state < 4:
-			state += 1
+
+def b():
+	state = 0
+	final = [1]
+	alphabet1 = "_AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+	alphabet2 = "0123456789"
+	for c in stra:
+		if state == 0:
+			if c in alphabet1: #is a letter
+				state = 1
+			else:
+				return False
+		elif state == 1:
+			if c not in alphabet1 and c not in alphabet2: #not a letter or digit
+				return False
 	if state in final:
 		return True
 	return False
-	
-def e(str):
-	state = 0;
+
+def c():
+	state = 0
 	final = [3]
-	for c in str:
-		if c not in alphabet:
-			return False
-		if state < 4:
-			state += 1
+	alphabet = "0123456789AaBbCcDdEeFf"
+	for c in stra:
+		if state == 0:
+			if c == '0':
+				state = 1
+			else:
+				return False
+		elif state == 1:
+			if c == 'x' or c == 'X':
+				state = 2
+			else:
+				return False
+		elif state == 2:
+			if c in alphabet:
+				state = 3
+			else:
+				return False
+		elif state == 3:
+			if c not in alphabet:
+				return False
 	if state in final:
-		return False
-	return True
-	
-str = input("Input a string: ")
-if a(str):
-	print("Valid string ending in ab")
-if b(str):
-	print("Valid string containing 3 consecutive a's")
-if c(str):
-	print("Valid string starting and ending with the same symbol")
-if d(str):
-	print("Valid string with three symbols")
-if e(str):
-	print("Valid string that is not three symbols")
+		return True
+	return False
+
+def d():
+	state = 0
+	final = [2]
+	for c in stra:
+		if state == 0:
+			if c == '"':
+				state = 1
+			else:
+				return False
+		elif state == 1:
+			if c == '"':
+				state = 2
+			elif c == '\\':
+				state = 3
+		elif state == 2:
+			return False
+		elif state == 3:
+			state = 1
+	if state in final:
+		return True
+	return False
+
+def e():
+	state = 0
+	final = [8, 10]
+	numbers = "0123456789"
+	for c in stra:
+		if state == 0:
+			if c == '1':
+				state = 1
+			elif c == '0':
+				state = 11
+			else:
+				return False
+		elif state == 1:
+			if c in "012":
+				state = 2
+			else:
+				return False
+		elif state == 2:
+			if c  == '/':
+				state = 3
+			else:
+				return False
+		elif state == 3:
+			if c in "12":
+				state = 4
+			elif c == '0':
+				state = 12
+			elif c == '3':
+				state = 13
+			else:
+				return False
+		elif state == 4:
+			if c in numbers:
+				state = 5
+			else:
+				return False
+		elif state == 5:
+			if c == '/':
+				state = 6
+			else:
+				return False
+		elif state == 6:
+			if c in numbers:
+				state = 7
+			else:
+				return False
+		elif state == 7:
+			if c in numbers:
+				state = 8
+			else:
+				return False
+		elif state == 8:
+			if c in numbers:
+				state = 9
+			else:
+				return False
+		elif state == 9:
+			if c in numbers:
+				state = 10
+			else:
+				return False
+		elif state == 10:
+			return False
+		elif state == 11:
+			if c in "123456789":
+				state = 2
+			else:
+				return False
+		elif state == 12:
+			if c in "123456789":
+				state = 5
+			else:
+				return False
+		elif state == 13:
+			if c in "01":
+				state = 5
+			else:
+				return False
+	if state in final:
+		return True
+	return False
+
+
+stra = input("Input string: ")
+if a():
+	print("Valid C-style comment.")
+if b():
+	print("Valid C identifier.")
+if c():
+	print("Valid C hexadecimal.")
+if d():
+	print("Valid C string constant.")
+if e():
+	print("Valid date.")
